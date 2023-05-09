4f8b7b456e3488982485cc7dc43d9f79de26d065
Konrad <konrad.dabrowski02@gmail.com>
2023-05-09 02:57:04 +0200

    Change to fetching structure. Adding variables so you can use it with other tickers.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import json
+import requests
 
-# Read the JSON data from the 'holders.json' file
-with open('holders.json', 'r') as json_file:
-    json_data = json.load(json_file)
+# Set the URL template
+url_template = "https://unisat.io/brc20-api-v2/brc20/{ticker}/holders?start=0&limit={numberOfHolders}"
+
+# Replace {ticker} and {numberOfHolders} with the actual values you can see the holders on unisat.io
+ticker = "subC"
+number_of_holders = 336
+
+# Format the URL with the actual values
+url = url_template.format(ticker=ticker, numberOfHolders=number_of_holders)
+
+# Fetch the JSON data from the URL
+response = requests.get(url)
+json_data = response.json()
 
 # Extract the addresses
 addresses = [detail['address'] for detail in json_data['data']['detail']]
